7376b5d6473c687294f1bec4fe13a75b898fcb34
Dewald Els <dewald@sumodevelopment.co.za>
2016-11-01 17:01:09 +0200

    Tiles module
    Added tiles module

---
+++ application/views/modules/tiles/featured/index.php
@@ -0,0 +1,49 @@
+<div id="mod-tiles-featured" class="container">
+    <div class="row">
+        <div class="col-xs-6 col-sm-3">
+            <div class="tile blue">
+                <a href="">
+                    <img class="img-responsive" src="<?php echo base_url($this->resources->data['page']->modules['featuredTiles']->data->tiles[0]->src); ?>" alt="">
+                </a>
+            </div>
+        </div>
+        <div class="col-xs-6 col-sm-3">
+            <div class="tile orange">
+                <a href="">
+                    <img class="img-responsive" src="<?php echo base_url($this->resources->data['page']->modules['featuredTiles']->data->tiles[1]->src); ?>" alt="">
+                </a>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-6">
+            <div class="tile">
+                <a href="">
+                    <img class="img-responsive" src="<?php echo base_url($this->resources->data['page']->modules['featuredTiles']->data->tiles[2]->src); ?>" alt="">
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-xs-6 col-sm-3">
+            <div class="tile">
+                <a href="">
+                    <img class="img-responsive" src="<?php echo base_url($this->resources->data['page']->modules['featuredTiles']->data->tiles[3]->src); ?>" alt="">
+                </a>
+            </div>
+        </div>
+        <div class="col-xs-6 col-sm-3">
+            <div class="tile">
+                <a href="">
+                    <img class="img-responsive" src="<?php echo base_url($this->resources->data['page']->modules['featuredTiles']->data->tiles[4]->src); ?>" alt="">
+                </a>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-6">
+            <div class="tile">
+                <a href="">
+                    <img class="img-responsive" src="<?php echo base_url($this->resources->data['page']->modules['featuredTiles']->data->tiles[5]->src); ?>" alt="">
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
