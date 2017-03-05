3019ae808d177ec6f3e8a03178f6cadee0cb4963
Dewald Els <dewald@sumodevelopment.co.za>
2017-03-05 21:30:29 +0200

    Updated DB
    Updates to DB

--- data/puretours_2017-03-05.sql
+++ data/puretours_2017-03-05.sql
@@ -5,9 +5,9 @@
 # http://www.sequelpro.com/
 # https://github.com/sequelpro/sequelpro
 #
-# Host: 127.0.01 (MySQL 5.6.28)
+# Host: 127.0.0.1 (MySQL 5.6.28)
 # Database: puretours
-# Generation Time: 2017-03-05 09:37:05 +0000
+# Generation Time: 2017-03-05 19:30:17 +0000
 # ************************************************************
 
 
@@ -62,9 +62,9 @@ LOCK TABLES `menu_item` WRITE;
 INSERT INTO `menu_item` (`menu_item_id`, `name`, `display`, `page_id`, `menu_id`)
 VALUES
 	(1,'Home','Home',1,1),
-	(2,'Pure Skiing','Pure Skiing',1,1),
-	(3,'Pure Sport','Pure Sport',1,1),
-	(4,'Robinson Club','Robinson Club',1,1),
+	(2,'Pure Skiing','Pure Skiing',4,1),
+	(3,'Pure Sport','Pure Sport',5,1),
+	(4,'Robinson Club','Robinson Club',6,1),
 	(5,'About','About',2,1),
 	(6,'Contact','Contact',3,1);
 
@@ -99,7 +99,9 @@ VALUES
 	(4,'Header',3,'modules/header','header',1,'2016-10-26 00:12:27'),
 	(5,'Footer',4,'modules/footer','footer',1,'2016-10-26 00:12:56'),
 	(6,'Nav - Public',5,'modules/nav-public','navPublic',1,'2016-10-26 22:50:36'),
-	(7,'Tiles - Featured',6,'modules/tiles/featured','featuredTiles',1,'2016-11-04 15:52:08');
+	(7,'Tiles - Featured',6,'modules/tiles/featured','featuredTiles',1,'2016-11-04 15:52:08'),
+	(8,'Top 5 Locations',7,'modules/sliders/top5locations','top5Locations',1,'2017-03-05 14:01:52'),
+	(9,'Tiles - 2 Col News',6,'modules/tiles/twocolnews','twoColNews',1,'2017-03-05 20:11:08');
 
 /*!40000 ALTER TABLE `module` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -126,7 +128,8 @@ VALUES
 	(3,'Header'),
 	(4,'Footer'),
 	(5,'Navbar'),
-	(6,'Tiles');
+	(6,'Tiles'),
+	(7,'Slider');
 
 /*!40000 ALTER TABLE `module_categories` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -143,20 +146,22 @@ CREATE TABLE `page` (
   `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `slug` varchar(128) DEFAULT NULL,
   `active` tinyint(1) DEFAULT '1',
+  `keywords` varchar(128) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`page_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 LOCK TABLES `page` WRITE;
 /*!40000 ALTER TABLE `page` DISABLE KEYS */;
 
-INSERT INTO `page` (`page_id`, `page_title`, `date_created`, `slug`, `active`)
+INSERT INTO `page` (`page_id`, `page_title`, `date_created`, `slug`, `active`, `keywords`, `description`)
 VALUES
-	(1,'Home','2016-10-26 00:11:18','home',1),
-	(2,'About','2016-10-27 15:33:11','about',1),
-	(3,'Contact','2016-10-27 15:33:34','contact',1),
-	(4,'Pure Skiing','2016-11-01 15:12:19','pure-skiing',1),
-	(5,'Pure Sport','2016-11-01 15:12:25','pure-sport',1),
-	(6,'Robinson Club','2016-11-01 15:12:34','robinson-club',1);
+	(1,'Home','2016-10-26 00:11:18','home',1,'puretours,sport,ski','PureTours sport and ski trips'),
+	(2,'About','2016-10-27 15:33:11','about',1,NULL,NULL),
+	(3,'Contact','2016-10-27 15:33:34','contact',1,NULL,NULL),
+	(4,'Pure Skiing','2016-11-01 15:12:19','pure-skiing',1,NULL,NULL),
+	(5,'Pure Sport','2016-11-01 15:12:25','pure-sport',1,NULL,NULL),
+	(6,'Robinson Club','2016-11-01 15:12:34','robinson-club',1,NULL,NULL);
 
 /*!40000 ALTER TABLE `page` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -173,27 +178,43 @@ CREATE TABLE `page_module` (
   `module_id` int(11) unsigned DEFAULT NULL,
   `module_data` text,
   `order` int(2) DEFAULT '0',
+  `active` tinyint(1) DEFAULT '1',
+  `plugins` text,
   PRIMARY KEY (`page_module_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 LOCK TABLES `page_module` WRITE;
 /*!40000 ALTER TABLE `page_module` DISABLE KEYS */;
 
-INSERT INTO `page_module` (`page_module_id`, `page_id`, `module_id`, `module_data`, `order`)
+INSERT INTO `page_module` (`page_module_id`, `page_id`, `module_id`, `module_data`, `order`, `active`, `plugins`)
 VALUES
-	(1,1,3,'{\"src\": \"assets/img/backgrounds/modules/banner-home.jpg\"}',2),
-	(2,1,6,NULL,1),
-	(3,1,4,'{\"tel\":\"0861 123 754\"}',0),
-	(4,1,5,NULL,50),
-	(5,2,2,'{\"src\": \"assets/img/backgrounds/modules/banner-about.jpg\"}',2),
-	(6,2,6,NULL,1),
-	(7,2,4,'{\"tel\":\"0861 123 754\"}',0),
-	(8,2,5,NULL,50),
-	(9,1,7,'{\"tiles\": [{\"src\":\"assets/img/tiles/all-inclusive.jpg\"},{\"src\":\"assets/img/tiles/full-board.jpg\"},{\"src\":\"assets/img/tiles/wide-hosted.jpg\"},{\"src\":\"assets/img/tiles/beach.jpg\"},{\"src\":\"assets/img/tiles/ski-chalet.jpg\"},{\"src\":\"assets/img/tiles/sports.jpg\"}]\r\n}',3),
-	(10,3,3,'{\"src\": \"assets/img/backgrounds/modules/banner-home.jpg\"}',2),
-	(11,3,6,NULL,1),
-	(12,3,4,'{\"tel\": \"0419221111\"}',0),
-	(13,3,5,NULL,50);
+	(1,1,3,'{\"src\": \"assets/img/backgrounds/modules/banner-home.jpg\"}',2,1,NULL),
+	(2,1,6,NULL,1,1,NULL),
+	(3,1,4,'{\"tel\":\"0861 123 754\"}',0,1,NULL),
+	(4,1,5,NULL,50,1,NULL),
+	(5,2,2,'{\"src\": \"assets/img/backgrounds/modules/banner-about.jpg\"}',2,1,NULL),
+	(6,2,6,NULL,1,1,NULL),
+	(7,2,4,'{\"tel\":\"0861 123 754\"}',0,1,NULL),
+	(8,2,5,NULL,50,1,NULL),
+	(9,1,7,'{\"tiles\": [{\"src\":\"assets/img/tiles/all-inclusive.jpg\"},{\"src\":\"assets/img/tiles/full-board.jpg\"},{\"src\":\"assets/img/tiles/wide-hosted.jpg\"},{\"src\":\"assets/img/tiles/beach.jpg\"},{\"src\":\"assets/img/tiles/ski-chalet.jpg\"},{\"src\":\"assets/img/tiles/sports.jpg\"}]\r\n}',3,1,NULL),
+	(10,3,3,'{\"src\": \"assets/img/backgrounds/modules/banner-home.jpg\"}',2,1,NULL),
+	(11,3,6,NULL,1,1,NULL),
+	(12,3,4,'{\"tel\": \"0419221111\"}',0,1,NULL),
+	(13,3,5,NULL,50,1,NULL),
+	(14,1,8,'{\"slides\": [{\"src\" : \"assets/img/sliders/top5locations/slide1.jpg\",\"title\" : \"Italy\",\"text\" : \"Text about Italy here. Modern Italian cuisine has developed through centuries of social and political changes, with roots as far back as the 4th century BC. Italian cuisine in itself takes heavy influences, including Etruscan, and Jewish.\",\"button\" : {\"text\":\"See Italian escapes\",\"link\": \"/destination/italy\"}}, {\"src\" : \"assets/img/sliders/top5locations/slide2.jpg\",\"title\" : \"Germany\",\"text\" : \"Text about Germany here. Modern German cuisine has developed through centuries of social and political changes, with roots as far back as the 4th century BC. Italian cuisine in itself takes heavy influences, including Etruscan, and Jewish.\",\"button\" :{\"text\":\"See German escapes\",\"link\": \"/destination/germany\"}}]}',4,1,'{\"js\":[\"assets/plugins/unslider/js/unslider-min.js\",\"application/views/modules/sliders/top5locations/script.js\"],\"css\": [\"assets/plugins/unslider/css/unslider.css\",\"assets/plugins/unslider/css/unslider-dots.css\"]}'),
+	(15,4,4,'{\"tel\":\"0418887777\"}',0,1,NULL),
+	(16,4,3,'{\"src\": \"assets/img/backgrounds/modules/banner-home.jpg\"}',2,1,NULL),
+	(17,4,6,NULL,1,1,NULL),
+	(18,4,5,NULL,50,1,NULL),
+	(19,5,3,'{\"src\": \"assets/img/backgrounds/modules/banner-home.jpg\"}',2,1,NULL),
+	(20,5,4,'{\"tel\":\"0713334444\"}',0,1,NULL),
+	(21,5,6,NULL,1,1,NULL),
+	(22,5,5,NULL,50,1,NULL),
+	(23,6,3,'{\"src\": \"assets/img/backgrounds/modules/banner-home.jpg\"}',2,1,NULL),
+	(24,6,4,'{\"tel\":\"0713335555\"}',0,1,NULL),
+	(25,6,6,NULL,1,1,NULL),
+	(26,6,5,NULL,50,1,NULL),
+	(27,1,9,'{\"tiles\":[{\"src\":\"assets/img/tiles/twocolnews/tile1.jpg\",\"link\":\"link/value\"},{\"src\":\"assets/img/tiles/twocolnews/tile2.jpg\",\"link\":\"link/value\"},{\"src\":\"assets/img/tiles/twocolnews/tile3.jpg\",\"link\":\"link/value\"},{\"src\":\"assets/img/tiles/twocolnews/tile4.jpg\",\"link\":\"link/value\"},{\"src\":\"assets/img/tiles/twocolnews/tile5.jpg\",\"link\":\"link/value\"},{\"src\":\"assets/img/tiles/twocolnews/tile6.jpg\",\"link\":\"link/value\"}],\"content\":{\"top\":{\"title\":\"Discover Pure Tours\",\"text\":\"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pretium ut justo at scelerisque. Donec feugiat ligula nec nisl ornare viverra. Aliquam accumsan risus purus, id hendrerit velit dictum at.\\n\\nMauris eget felis ex. Proin pharetra non purus et posuere. Donec elementum sit amet turpis quis molestie. Cras posuere commodo dolor, eget porttitor ante sollicitudin et. Suspendisse at posuere ex. Lorem ipsum dolor sit amet, consectetur adipiscing elit.\\n\\nMaecenas pretium ut justo at scelerisque. Donec feugiat ligula nec nisl ornare viverra. Aliquam accumsan risus purus, id hendrerit velit dictum at. Mauris eget felis ex. Proin pharetra non purus et posuere. Donec elementum sit amet turpis quis molestie. Cras posuere commodo dolor, eget porttitor ante sollicitudin et. Suspendisse at posuere ex. Cras posuere commodo dolor, eget porttitor ante sollicitudin et. Cras posuere commodo dolor, eget porttitor ante sollicitudi.\",\"button\":{\"text\":\"Find out more about us\",\"link\":\"about\"}},\"bottom\":{\"title\":\"Latest travel News\",\"button\":{\"text\":\"Read all travel news\",\"link\":\"travel-news\"}}}}',5,1,NULL);
 
 /*!40000 ALTER TABLE `page_module` ENABLE KEYS */;
 UNLOCK TABLES;
